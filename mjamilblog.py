7ef83e3fea3c1bb9a01da72d008a88a88e62b74e
MohamadJamil <mjamil@kent.edu>
2019-10-21 21:34:16 -0400

    error handeling

--- mjamilblog.py
+++ mjamilblog.py
@@ -1,6 +1,10 @@
 from app import app, db
 from app.models import User, Post
-
+app = Flask(__name__)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ start.sh
@@ -0,0 +1,8 @@
+# export FLASK_APP=mjamil.py
+# export Flask_DEBUG=0
+# export MAIL_SERVER=smtp.googlemail.com
+# export MAIL_PORT=587
+# export MAIL_USE_TLS=1
+# export MAIL_USERNAME = os.environ.get('GMAIL_EMAIL')
+# export MAIL_PASSWORD = os.environ.get('GMAIL_PASS')
+# flask run
